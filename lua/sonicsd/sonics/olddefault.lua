d71db35e6601a762a977a303474f6641b4544613
Matt Jeanes <mattjeanes23@gmail.com>
2024-01-03 02:35:32 +0000

    add old default sonics

---
+++ lua/sonicsd/sonics/olddefault.lua
@@ -0,0 +1,37 @@
+SonicSD:AddSonic({
+    ID = "olddefault",
+    Name = "2010 Screwdriver",
+    ViewModel = "models/weapons/c_sonicsd.mdl",
+    WorldModel = "models/weapons/w_sonicsd.mdl",
+    LightPos = Vector(20,-1.75,-2.75),
+    LightBrightness = 5,
+    SoundLoop = "sonicsd/loop_2010_1.wav",
+    SoundLoop2 = "sonicsd/loop_2010_2.wav",
+    DefaultLightColor = Color(50, 250, 50),
+    DefaultLightColor2 = Color(0, 200, 100),
+    DefaultLightColorOff = Color(0, 80, 0)
+})
+
+SonicSD:AddSonic({
+    ID = "10th-slogan",
+    Name = "2005 Screwdriver",
+    ViewModel = "models/doctor_who/sonic_screwdriver/c_10thsonicsd.mdl",
+    WorldModel = "models/doctor_who/sonic_screwdriver/w_10thsonicsd.mdl",
+    LightPos = Vector(20,-2.5,-3.15),
+    LightBrightness = 2,
+    SoundLoop = "sonicsd/loop_2005_1.wav",
+    SoundLoop2 = "sonicsd/loop_2005_2.wav",
+    ButtonSoundOff = "sonicsd/button_off_2.wav",
+    DefaultLightColor = Color(0, 20, 200),
+    DefaultLightColor2 = Color(0, 100, 200)
+})
+
+SonicSD:AddSonic({
+    ID = "4th-slogan",
+    Name = "1968 Screwdriver",
+    ViewModel = "models/doctor_who/sonic_screwdriver/c_4thsonicsd.mdl",
+    WorldModel = "models/doctor_who/sonic_screwdriver/w_4thsonicsd.mdl",
+    SoundLoop = "sonicsd/loop_1968_1.wav",
+    SoundLoop2 = "sonicsd/loop_1968_2.wav",
+    LightDisabled = true
+})
